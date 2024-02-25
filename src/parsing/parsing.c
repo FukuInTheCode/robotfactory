a0bc5f77cd31e4845a646a31dadeb1e0ec64d605
Tom C <tom.carlier@epitech.eu>
2024-02-25 19:12:15 +0100

    [refactor] add the parsing functions with basic return

---
+++ src/parsing/parsing.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** parsing.c
+** File description:
+** parsing.c
+*/
+
+#include "my.h"
+
+int parsing(FILE *asmbly, FILE *bin)
+{
+    if (!asmbly || !bin)
+        return 84;
+    return 0;
+}
