92fe066225ba30b2073a72522599d0fa6d688cf9
Tom C <tom.carlier@epitech.eu>
2024-02-25 19:45:51 +0100

    [refactor] add the read_file function that read the asm file line by line

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -7,9 +7,21 @@
 
 #include "my.h"
 
+static int read_files(FILE *asmbly, FILE *bin)
+{
+    char *line = NULL;
+    size_t len = 0;
+
+    for (int len = getline(&line, &len, asmbly); len != -1;
+        len = getline(&line, &len, asmbly))
+            if (!handle_line(line, len))
+                return 84;
+    return 0;
+}
+
 int parsing(FILE *asmbly, FILE *bin)
 {
     if (!asmbly || !bin)
         return 84;
-    return 0;
+    return read_files(asmbly, bin);
 }
