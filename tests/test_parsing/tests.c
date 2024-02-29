4221b6493d372ea753f9a8f026b1679ae84bc2da
Tom C <tom.carlier@epitech.eu>
2024-02-29 09:31:45 +0100

    [feat] add the tests.c for the test_parsing

---
+++ tests/test_parsing/tests.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** tests.c
+** File description:
+** tests.c
+*/
+
+#include "my.h"
+
+int main(void)
+{
+    if (parsing(NULL, NULL) != 84)
+        return 84;
+    return 0;
+}
