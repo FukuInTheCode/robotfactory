f7880a849b592e677ff11bbef69d21f05a1372d8
Tom C <tom.carlier@epitech.eu>
2024-02-29 09:21:07 +0100

    [feat] add the label_t struct that represent a label and it position in the file

--- include/robot_factory.h
+++ include/robot_factory.h
@@ -11,13 +11,18 @@
     #include <stdlib.h>
     #include <unistd.h>
 
+typedef struct label_s {
+    char *name;
+    int position;
+} label_t;
+
 void my_putchar(char c);
 char **my_str_to_word_array(char const *);
 int my_free_str_array(char **);
 
-int parsing(FILE *, FILE *);
+int parsing(FILE *, FILE *, label_t *);
 
-typedef bool(*is_func)(int, char **, FILE *);
+typedef bool(*is_func)(char **, FILE *, label_t *, int);
 
 static const is_func is_functions_array[] = {
     NULL
