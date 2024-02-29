50c7f050e7eebbc831276cdf375ff0ccec9bfb3b
Tom C <tom.carlier@epitech.eu>
2024-02-29 09:18:53 +0100

    [refactor] now add the use of a labels_t struct

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -7,22 +7,13 @@
 
 #include "my.h"
 
-static int get_str_array_len(char **)
-{
-    size_t len = 0;
-
-    for (; array[len]; len++);
-    return len;
-}
-
-static bool handle_line(char const *line, FILE *bin)
+static bool handle_line(char const *line, FILE *bin, labels_t *labels, int pos)
 {
     int is_nbr = 0;
     char **argv = my_str_to_word_array(line);
-    int argc = get_str_array_len(argv);
 
     for (uint8_t i = 0; is_functions_array[i]; i++) {
-        is_nbr += is_functions_array[i](argc, argv, bin);
+        is_nbr += is_functions_array[i](argv, bin, labels, pos);
         if (is_nbr > 1)
             return false;
     }
@@ -30,21 +21,25 @@ static bool handle_line(char const *line, FILE *bin)
     return true;
 }
 
-static int read_files(FILE *asmbly, FILE *bin)
+static int read_files(FILE *asmbly, FILE *bin, labels_t *labels)
 {
     char *line = NULL;
     size_t len = 0;
+    int position = 0;
 
     for (int len = getline(&line, &len, asmbly); len != -1;
-        len = getline(&line, &len, asmbly))
-            if (!handle_line(line, bin))
+        len = getline(&line, &len, asmbly)) {
+            position += len - 1;
+            line[len] = 0;
+            if (!handle_line(line, bin, labels, position))
                 return 84;
+    }
     return 0;
 }
 
-int parsing(FILE *asmbly, FILE *bin)
+int parsing(FILE *asmbly, FILE *bin, labels_t *labels)
 {
     if (!asmbly || !bin)
         return 84;
-    return read_files(asmbly, bin);
+    return read_files(asmbly, bin, labels);
 }
