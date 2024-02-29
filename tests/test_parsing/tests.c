80a4a6d97aecddc122d5dac4cb7f3ac667afc714
Tom C <tom.carlier@epitech.eu>
2024-02-29 09:40:54 +0100

    [feat] add some test for the test_parsing

--- tests/test_parsing/tests.c
+++ tests/test_parsing/tests.c
@@ -11,5 +11,9 @@ int main(void)
 {
     if (parsing(NULL, NULL) != 84)
         return 84;
+    FILE *file = fopen("../champions/champion.s", "r");
+    FILE *bin = fopen("tmp.cor", "w");
+    if (parsing(file, bin) != 84)
+        return 84;
     return 0;
 }
