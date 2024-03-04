567806196e41eed72415717911b1d77620197701
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-04 13:05:06 +0100

    [style] --> fix coding style

--- src/error_handling/error_handling.c
+++ src/error_handling/error_handling.c
@@ -7,9 +7,25 @@
 
 #include "my.h"
 
-int robot_factory(int argc, char **argv)
+
+
+int open_file(char *path)
 {
-    if (argc != 2)
+    int fd = open(path, O_RDONLY);
+    bool flag = false;
+
+    if (fd == -1)
         return 84;
+    for (int i = 0; path[i] != '\0'; i++) {
+        if (!flag && path[i] == '.' && path[i + 1] == 's') {
+            flag == true;
+            return 0;
+        }
+    }
+    if (flag == false) {
+        close(fd);
+        return 84;
+    }
+    close(fd);
     return 0;
 }
