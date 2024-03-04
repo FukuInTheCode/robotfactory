65b9a20673c54445fef68c4e5f7725a2af96d2db
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-04 17:46:34 +0100

    [feat] -->  add the function for write if its live

---
+++ src/parsing/write/write_live.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** write_live.c
+** File description:
+** write in bin for 'live'
+*/
+
+#include "my.h"
+
+int write_live(char **argv, FILE *bin)
+{
+    unsigned char indicator = 0x01;
+    unsigned int nbr = my_getnbr(argv[1] + 1);
+
+    fwrite(&indicator, sizeof(indicator), 1, bin);
+    fwrite(&nbr, sizeof(nbr), 1, bin);
+    return 0;
+}
