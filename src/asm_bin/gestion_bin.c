06a57ebe5cfe189619fda917e7ea76a21fd16d9c
Tom C <tom.carlier@epitech.eu>
2024-03-04 15:31:13 +0100

    [fix] invalid read size 1 in bin

--- src/asm_bin/gestion_bin.c
+++ src/asm_bin/gestion_bin.c
@@ -11,6 +11,7 @@ int create_bin(FILE *asmbly, char *name_champ)
 {
     FILE *bin;
 
+    name_champ = realloc(name_champ, my_strlen(name_champ) + 5);
     my_strcat(name_champ, ".cor");
     bin = fopen(name_champ, "w");
     if (parsing(asmbly, bin) == 84)
