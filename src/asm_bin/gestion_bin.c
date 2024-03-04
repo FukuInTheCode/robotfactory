41e6fc2addceb2b4bc5f58fe0d6e756c2b852dfc
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-04 15:41:16 +0100

    [feat] -->  now writes the bytes in reverse

--- src/asm_bin/gestion_bin.c
+++ src/asm_bin/gestion_bin.c
@@ -10,11 +10,14 @@
 int create_bin(FILE *asmbly, char *name_champ)
 {
     FILE *bin;
+    unsigned int magic = COREWAR_EXEC_MAGIC;
 
+    magic = my_revbyte(magic);
     my_strcat(name_champ, ".cor");
     bin = fopen(name_champ, "wb");
     if (bin == NULL)
         return 84;
+    fwrite(&magic, sizeof(unsigned int), 1, bin);
     if (parsing(asmbly, bin) == 84)
         return 84;
     fclose(bin);
