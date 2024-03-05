44570f554e82faa5cb54a0c16d0b3db03ca00052
Tom C <tom.carlier@epitech.eu>
2024-03-05 22:54:26 +0100

    [fix] fix some thingy

--- src/my/code_byte.c
+++ src/my/code_byte.c
@@ -9,11 +9,5 @@
 
 uint8_t get_coding_byte(arg_t arg1, arg_t arg2, arg_t arg3, arg_t arg4)
 {
-    uint8_t coding_byte = 0;
-
-    coding_byte |= (arg1 & 0x03);
-    coding_byte |= (arg2 & 0x03) << 2;
-    coding_byte |= (arg3 & 0x03) << 4;
-    coding_byte |= (arg4 & 0x03) << 6;
-    return coding_byte;
+    return (arg1 << 6) | (arg2 << 4) | (arg3 << 2) | arg4;
 }
--- src/my/my_revbyte.c
+++ src/my/my_revbyte.c
@@ -20,7 +20,7 @@ uint8_t my_revbyte_8(uint8_t num)
 
 uint16_t my_revbyte_16(uint16_t num)
 {
-    return ((num & 0xFF) << 8) | ((num >> 8) & 0xFF);
+    return (num << 8) | (num >> 8);
 }
 
 uint32_t my_revbyte_32(uint32_t num)
