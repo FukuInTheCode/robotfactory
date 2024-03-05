f343cd103b7169c5a63e0ea05fb45939bfb7e3a5
Tom C <tom.carlier@epitech.eu>
2024-03-05 23:13:53 +0100

    [fix] add

--- src/parsing/write/write_add.c
+++ src/parsing/write/write_add.c
@@ -6,17 +6,20 @@
 */
 
 #include "my.h"
+#include <stdint.h>
+
+// coding byte a mettre va comprendre .??
 
 int write_add(char **argv, FILE *bin)
 {
     unsigned char indicator = 0x04;
-    unsigned int nbr = my_revbyte_32(my_getnbr(argv[1] + 1));
-    unsigned int nbr2 = my_revbyte_32(my_getnbr(argv[2] + 1));
-    unsigned int nbr3 = my_revbyte_32(my_getnbr(argv[3] + 1));
+    uint8_t nbr = my_getnbr(argv[1] + 1);
+    uint8_t nbr2 = my_getnbr(argv[2] + 1);
+    uint8_t nbr3 = my_getnbr(argv[3] + 1);
 
     fwrite(&indicator, sizeof(indicator), 1, bin);
-    fwrite(&nbr, sizeof(nbr), 1, bin);
-    fwrite(&nbr2, sizeof(nbr2), 1, bin);
-    fwrite(&nbr3, sizeof(nbr3), 1, bin);
+    fwrite(&nbr, sizeof(uint8_t), 1, bin);
+    fwrite(&nbr2, sizeof(uint8_t), 1, bin);
+    fwrite(&nbr3, sizeof(uint8_t), 1, bin);
     return 0;
 }
