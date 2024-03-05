cfd394bb44b8b39369bfca336b8174ca9717f411
Tom C <tom.carlier@epitech.eu>
2024-03-05 22:54:43 +0100

    [refactor] fisdg

--- src/parsing/write/write_ld.c
+++ src/parsing/write/write_ld.c
@@ -10,9 +10,9 @@
 int write_ld(char **argv, FILE *bin)
 {
     unsigned char indicator = 0x02;
-    unsigned int coding_byte;
-    unsigned int nbr = my_revbyte(my_getnbr(argv[1]));
-    unsigned int nbr2 = my_revbyte(my_getnbr(argv[2] + 1));
+    uint8_t coding_byte;
+    uint16_t nbr = my_revbyte_16(my_getnbr(argv[1]));
+    uint8_t nbr2 = my_getnbr(argv[2] + 1);
 
     fwrite(&indicator, sizeof(indicator), 1, bin);
     if (is_direct(argv[1])) {
@@ -23,7 +23,7 @@ int write_ld(char **argv, FILE *bin)
         coding_byte = get_coding_byte(INDIRECT, REGISTRE, OTHER, OTHER);
         fwrite(&coding_byte, sizeof(coding_byte), 1, bin);
     }
-    fwrite(&nbr, sizeof(nbr), 1, bin);
+    fwrite(&nbr, sizeof(uint16_t), 1, bin);
     fwrite(&nbr2, sizeof(nbr2), 1, bin);
     return 0;
 }
--- src/parsing/write/write_sub.c
+++ src/parsing/write/write_sub.c
@@ -10,9 +10,9 @@
 int write_sub(char **argv, FILE *bin)
 {
     unsigned char indicator = 0x05;
-    unsigned int nbr = my_revbyte(my_getnbr(argv[1] + 1));
-    unsigned int nbr2 = my_revbyte(my_getnbr(argv[2] + 1));
-    unsigned int nbr3 = my_revbyte(my_getnbr(argv[3] + 1));
+    unsigned int nbr = my_revbyte_32(my_getnbr(argv[1] + 1));
+    unsigned int nbr2 = my_revbyte_32(my_getnbr(argv[2] + 1));
+    unsigned int nbr3 = my_revbyte_32(my_getnbr(argv[3] + 1));
 
     fwrite(&indicator, sizeof(indicator), 1, bin);
     fwrite(&nbr, sizeof(nbr), 1, bin);
