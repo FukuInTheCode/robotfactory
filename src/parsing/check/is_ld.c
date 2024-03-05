bdd84a3b597227cc28f68d0de6317ac9ef432ab9
Tom C <tom.carlier@epitech.eu>
2024-03-05 19:41:10 +0100

    [fix]is_ld

--- src/parsing/check/is_ld.c
+++ src/parsing/check/is_ld.c
@@ -9,7 +9,7 @@
 
 bool is_ld(char **argv, FILE *bin)
 {
-    if (argv[1] == NULL || argv[2] == NULL || !(my_strcmp("ld", argv[0])))
+    if (argv[1] == NULL || argv[2] == NULL || (my_strcmp("ld", argv[0])))
         return false;
     if (find_array_len(argv) == 3
         && (is_direct(argv[1]) || is_indirect(argv[1]))
