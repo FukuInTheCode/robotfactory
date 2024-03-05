e48903e4bdee68a08fac478650337067b8e1c556
Tom C <tom.carlier@epitech.eu>
2024-03-05 19:37:03 +0100

    ndjfddsq

--- src/my/code_byte.c
+++ src/my/code_byte.c
@@ -9,11 +9,11 @@
 
 uint8_t get_coding_byte(arg_t arg1, arg_t arg2, arg_t arg3, arg_t arg4)
 {
-    uint8_t ret = 0;
+    uint8_t coding_byte = 0;
 
-    ret = (ret >> 2) + arg4;
-    ret = (ret >> 2) + arg3;
-    ret = (ret >> 2) + arg2;
-    ret = (ret >> 2) + arg1;
-    return ret;
+    coding_byte |= (arg1 & 0x03);
+    coding_byte |= (arg2 & 0x03) << 2;
+    coding_byte |= (arg3 & 0x03) << 4;
+    coding_byte |= (arg4 & 0x03) << 6;
+    return coding_byte;
 }
