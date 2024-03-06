19bae719c902f8e471656916545fc5aae8d0ff3d
Tom C <tom.carlier@epitech.eu>
2024-03-06 18:27:59 +0100

    [refactor] -->  update file(s)

--- src/parsing/write/write_ld.c
+++ src/parsing/write/write_ld.c
@@ -6,18 +6,15 @@
 */
 
 #include "my.h"
+#include <stdint.h>
 
 // marche quand le premier arg est
 // un indirect mais doit changer quand c un dire direct
 
-int write_ld(char **argv, FILE *bin)
+static int write_coding(char **argv, FILE *bin)
 {
-    unsigned char indicator = 0x02;
     uint8_t coding_byte;
-    uint16_t nbr = my_revbyte_16(my_getnbr(argv[1]));
-    uint8_t nbr2 = my_getnbr(argv[2] + 1);
 
-    fwrite(&indicator, sizeof(indicator), 1, bin);
     if (is_direct(argv[1])) {
         coding_byte = get_coding_byte(DIRECT, REGISTRE, OTHER, OTHER);
         fwrite(&coding_byte, sizeof(coding_byte), 1, bin);
@@ -26,7 +23,23 @@ int write_ld(char **argv, FILE *bin)
         coding_byte = get_coding_byte(INDIRECT, REGISTRE, OTHER, OTHER);
         fwrite(&coding_byte, sizeof(coding_byte), 1, bin);
     }
-    fwrite(&nbr, sizeof(uint16_t), 1, bin);
-    fwrite(&nbr2, sizeof(nbr2), 1, bin);
+    return 0;
+}
+
+int write_ld(char **argv, FILE *bin)
+{
+    unsigned char indicator = 0x02;
+    uint32_t nbr = my_revbyte_16(my_getnbr(argv[1]));
+    uint8_t nbr2 = my_getnbr(argv[2] + 1);
+    uint8_t offset = 0;
+
+    fwrite(&indicator, sizeof(indicator), 1, bin);
+    write_coding(argv, bin);
+    if (is_direct(argv[1])) {
+        nbr = my_revbyte_32(my_getnbr(argv[1] + 1));
+        offset = 2;
+    }
+    fwrite(&nbr, sizeof(uint16_t) + offset, 1, bin);
+    fwrite(&nbr2, sizeof(uint8_t), 1, bin);
     return 0;
 }
