c73e89a1a42197d8fbd1c8f763d7e09be8cf8d8b
Tom C <tom.carlier@epitech.eu>
2024-03-06 17:55:37 +0100

    [refactor] -->  update file(s)

--- src/parsing/check/is_ldi.c
+++ src/parsing/check/is_ldi.c
@@ -11,7 +11,7 @@ bool is_ldi(char **argv, FILE *bin)
 {
     if (my_strcmp(argv[0], "ldi") != 0)
         return false;
-    if (find_array_len(argv) != 4
+    if (find_array_len(argv) == 4
         && (is_direct(argv[1]) || is_register(argv[1]))
         && (is_direct(argv[2]) || is_register(argv[2]))
         && is_register(argv[3])) {
