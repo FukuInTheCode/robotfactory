eb4b30159c435254349aea416b36af33eef4d308
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-06 19:27:36 +0100

    [refactor] -->  add coding byte

--- src/parsing/write/write_sub.c
+++ src/parsing/write/write_sub.c
@@ -13,11 +13,13 @@
 int write_sub(char **argv, FILE *bin)
 {
     unsigned char indicator = 0x05;
+    unsigned char coding_byte = 0x54;
     uint8_t nbr = my_getnbr(argv[1] + 1);
     uint8_t nbr2 = my_getnbr(argv[2] + 1);
     uint8_t nbr3 = my_getnbr(argv[3] + 1);
 
     fwrite(&indicator, sizeof(indicator), 1, bin);
+    fwrite(&coding_byte, sizeof(coding_byte), 1, bin);
     fwrite(&nbr, sizeof(uint8_t), 1, bin);
     fwrite(&nbr2, sizeof(uint8_t), 1, bin);
     fwrite(&nbr3, sizeof(uint8_t), 1, bin);
