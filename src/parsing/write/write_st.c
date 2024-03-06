5b17c2f449a0f6a8c3fa158825545f73700da351
Tom C <tom.carlier@epitech.eu>
2024-03-06 18:34:37 +0100

    [refactor] -->  update file(s)

--- src/parsing/write/write_st.c
+++ src/parsing/write/write_st.c
@@ -10,26 +10,35 @@
 
 // faire un sizeof(uint8_t) si argv[2] est un registre au
 // dernier write
-
-int write_st(char **argv, FILE *bin)
+static int write_coding(char **argv, FILE *bin)
 {
-    unsigned char indicator = 0x03;
     uint8_t coding_byte;
-    uint8_t nbr = my_getnbr(argv[1] + 1);
-    uint16_t nbr2;
 
-    fwrite(&indicator, sizeof(indicator), 1, bin);
     if (is_register(argv[2])) {
         coding_byte = get_coding_byte(REGISTRE, REGISTRE, OTHER, OTHER);
         fwrite(&coding_byte, sizeof(coding_byte), 1, bin);
-        nbr2 = my_revbyte_8(my_getnbr(argv[2] + 1));
     }
     if (is_indirect(argv[2])) {
         coding_byte = get_coding_byte(REGISTRE, INDIRECT, OTHER, OTHER);
         fwrite(&coding_byte, sizeof(coding_byte), 1, bin);
+    }
+    return 0;
+}
+
+int write_st(char **argv, FILE *bin)
+{
+    unsigned char indicator = 0x03;
+    uint8_t nbr = my_getnbr(argv[1] + 1);
+    uint16_t nbr2 = my_getnbr(argv[2] + 1);
+    uint8_t offset = 0;
+
+    fwrite(&indicator, sizeof(indicator), 1, bin);
+    write_coding(argv, bin);
+    if (is_indirect(argv[2])) {
         nbr2 = my_revbyte_16(my_getnbr(argv[2]));
+        offset = 1;
     }
     fwrite(&nbr, sizeof(uint8_t), 1, bin);
-    fwrite(&nbr2, sizeof(uint16_t), 1, bin);
+    fwrite(&nbr2, sizeof(uint8_t) + offset, 1, bin);
     return 0;
 }
