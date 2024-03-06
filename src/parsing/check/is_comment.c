8e02ae372130c69a7f556248e792cc1682f0e2ae
Tom C <tom.carlier@epitech.eu>
2024-03-06 17:34:02 +0100

    [fix] --> comment too long

--- src/parsing/check/is_comment.c
+++ src/parsing/check/is_comment.c
@@ -12,7 +12,7 @@ bool is_comment(char **argv, FILE *bin, header_t *header)
 {
     char **tmp = NULL;
 
-    if (find_array_len(argv) != 2)
+    if (find_array_len(argv) != 2 || my_strlen(argv[1]) > COMMENT_LENGTH)
         return false;
     tmp = my_str_to_word_array(*argv, " \t");
     if (find_array_len(tmp) != 1 || my_strcmp(*tmp, COMMENT_CMD_STRING))
